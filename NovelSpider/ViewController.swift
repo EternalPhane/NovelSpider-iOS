413889089dcb956bf906d88bdf81e91042a71222
左宗源 <eternalphane@zuozongyuans-macbook-pro.local>
2017-04-20 19:12:00 +0800

    Initial Commit

---
+++ NovelSpider/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NovelSpider
+//
+//  Created by 左宗源 on 2017/4/20.
+//  Copyright © 2017年 eternalphane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
